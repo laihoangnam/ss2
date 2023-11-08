7cbab0195e325243015b396a2cb25b7c9299337c
laihoangnam <147390420+laihoangnam@users.noreply.github.com>
2023-11-08 11:10:57 +0700

    Add files via upload

---
+++ dientich.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+	const float pi=3.14;
+	int a,b,c;
+	printf("nhap a:");
+	scanf("%d", &a);
+	
+    printf("nhap b:");
+	scanf("%d", &b);
+	
+	printf("nhap c:");
+	scanf("%d", &c);
+	
+	
+	int C=(a+b)*2;
+	int D=b*4;
+	int E=a*b;
+	int F=b*b;
+	int G=2*pi*c;
+	int H=c*c*pi;
+	printf("%d ", C);
+	
+	
+
+	return 0;
+}
